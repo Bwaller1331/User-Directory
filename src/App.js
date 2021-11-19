a5c5a9f344124a08173e60bb45f824bf0652d461
Brandon Waller <bwall1331@gmail.com>
2021-11-19 17:15:49 -0500

    added heroku ping code

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import Header from "./components/Header"
 import PersonList from "./components/PersonList"
 
+var http = require("http");
+setInterval(function() {
+    http.get("https://tech-blog-app-brandon.herokuapp.com/");
+}, 300000); // every 5 minutes (300000)
 
 function App() {
   return (<div>
